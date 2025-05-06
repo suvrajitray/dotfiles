3136ef7129d67debf50bdbff683f477d30004be3
Suvrajit Ray <suvra@Suvrajits-Mac-mini.local>
2025-05-06 16:49:13 +0530

    initial changes

--- .vimrc
+++ .vimrc
@@ -1,25 +1,23 @@
-" don't try to be vi comatible
-set nocompatible
-
-" set leader key to space
-let mapleader = " "
-
 " better escape
 inoremap jk <Esc>
 
-" enable clipboard support (requires +clipboard)
-set clipboard=unnamedplus
-
-" Yank to system clipboard using <leader>y
-nnoremap <leader>y "+yy
-vnoremap <leader>y "+y
-
-" paste from system clipboard using <leader>p
-nnoremap <leader>p "+p
-lnoremap <leader>p "+p
-
+" set leader key to space
+let mapleader = " "
 nnoremap <leader>w :w!<CR>
 map <leader>e :Ex<CR>
+
+" move up/down editor lines
+nnoremap j gj
+nnoremap k gk
+
+" disable highlights when you press <leader><cr>:
+map <silent> <leader><cr> :noh<cr> 
+
+" smart way to move between windows
+map <C-j> <C-W>j
+map <C-k> <C-W>k
+map <C-h> <C-W>h
+map <C-l> <C-W>l
 
 " turn on syntax highlighting
 syntax on
@@ -36,21 +34,29 @@ set copyindent
 set expandtab
 set noshiftround
 
+" cursor motion
+set scrolloff=9
+set backspace=indent,eol,start
+set matchpairs+=<:> " use % to jump between pairs
+
+" allow hidden buffers
+set hidden
+
+" status bar
+" set laststatus=2
+
+" last line
+set showmode
+set showcmd
+
 set hlsearch
 set incsearch
 set showmatch
 set smartcase
 
+set t_Co=256
 set background=dark
+set cursorline
 
-" note: turn it off if your terminal is very slow. it will show the command in right side.
-set showcmd
+" colorscheme habamax
 
-" disable highlights when you press <leader><cr>:
-map <silent> <leader><cr> :noh<cr> 
-
-" smart way to move between windows
-map <C-j> <C-W>j
-map <C-k> <C-W>k
-map <C-h> <C-W>h
-map <C-l> <C-W>l
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,63 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- better escape
+keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk", silent = true, noremap = true })
+keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode with kj", silent = true, noremap = true })
+
+-- Move to window using the <ctrl> hjkl keys
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- windows
+keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Resize window using <alt> arrow keys
+keymap.set("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+keymap.set("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+keymap.set("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+keymap.set("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- files
+keymap.set("n", "QQ", ":q!<enter>", { noremap = false })
+keymap.set("n", "WW", ":w!<enter>", { noremap = false })
+keymap.set("n", "E", "$", { noremap = false })
+keymap.set("n", "B", "^", { noremap = false })
+
+-- paste over currently selected text without yanking it
+keymap.set("v", "p", '"_dP')
+keymap.set("v", "P", '"_dp')
+
+-- map ctrl + a to select all
+keymap.set("n", "<C-a>", "ggVG", { desc = "select all", silent = true, noremap = true })
+
+keymap.set(
+  "n",
+  "<BS>",
+  "vi'",
+  { desc = "map backspace to select all inside single quot", silent = true, noremap = true }
+)
+
+keymap.set("n", "<CR>", 'vi"', { desc = "map enter to select all inside double quot", silent = true, noremap = true })
+
+keymap.set(
+  "n",
+  "x",
+  '"_x',
+  { desc = "delete a character without affecting the register", silent = true, noremap = true }
+)
+
+-- quit all with leader
+keymap.set("n", "<leader>qa", ":qa<enter>", { desc = "Quit All!", silent = true, noremap = false })
