2de81fd347d6521f5c6083d6f305b0081389b348
Suvrajit Ray <suvra@Suvrajits-Mac-mini.local>
2025-05-07 19:17:02 +0530

    yazi config added.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-    {
-        "craftzdog/solarized-osaka.nvim",
-        lazy = true,
-        -- priority = 1000,
-        opts = function()
-            return {
-                transparent = true,
-                styles = {
-                    sidebar = "transparent",
-                    floats = "transparent",
-                },
-            }
-        end,
-    },
+    -- {
+    --     "craftzdog/solarized-osaka.nvim",
+    --     lazy = true,
+    --     -- priority = 1000,
+    --     opts = function()
+    --         return {
+    --             transparent = true,
+    --             styles = {
+    --                 sidebar = "transparent",
+    --                 floats = "transparent",
+    --             },
+    --         }
+    --     end,
+    -- },
     {
         "catppuccin/nvim",
         opts = {
@@ -24,8 +24,8 @@ return {
     {
         "LazyVim/LazyVim",
         opts = {
-            -- colorscheme = "catppuccin-mocha",
-            colorscheme = "solarized-osaka",
+            colorscheme = "catppuccin-mocha",
+            -- colorscheme = "solarized-osaka",
         },
     },
 }
