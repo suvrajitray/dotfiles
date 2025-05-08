f1a4546a5e5d3b7412a8e46588066f7f28ec7991
Suvrajit Ray <suvra@Suvrajits-MacBook-Air.local>
2025-05-08 19:24:45 +0530

    workaround added for mason lsp issue.

---
+++ nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,6 @@
+-- HACK: Mason is pinned to version 1 for now: https://github.com/LazyVim/LazyVim/issues/6039
+-- :TODO: need to remove this file when fix is available.
+return {
+    { "mason-org/mason.nvim", version = "^1.0.0" },
+    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
